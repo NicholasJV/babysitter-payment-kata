34f593db8eeac54d08c8badce364d63aed853f31
Cory Koehler <corykoehler@Corys-MacBook-Pro.local>
2017-09-20 15:53:27 -0400

    add pre and post bedtime rate pay calculation capability

--- app/PayCalculator.js
+++ app/PayCalculator.js
@@ -1,8 +1,14 @@
-function CalculatePayment(startTime, endTime) {
-    var rate = 12;
+function CalculatePayment(startTime, bedTime, endTime) {
+    var baseRate = 12;
+    var bedTimeRate = 8;
     startTime = startTime.split(':');
+    bedTime = bedTime.split(':');
     endTime = endTime.split(':');
 
-    var totalTime = endTime[0] - startTime[0];
-    return totalTime * rate;
+    var preBedTimeHours = bedTime[0] - startTime[0];
+    var postBestTimeHours = endTime[0] - bedTime[0];
+    // var totalTime = endTime[0] - startTime[0];
+    var preBedTimePay = preBedTimeHours * baseRate;
+    var postBedTimePay = postBestTimeHours * bedTimeRate;
+    return preBedTimePay + postBedTimePay;
 }
--- test/PayCalculator_spec.js
+++ test/PayCalculator_spec.js
@@ -1,5 +1,10 @@
 describe("Pay Calculator", function() {
-    it("should calculate payment based on hours worked", function() {
-        expect(CalculatePayment('5:00', '8:00')).toBe(36);
+    
+    it("should calculate payment based on hours worked", function(){
+        expect(CalculatePayment('5:00', '8:00', '8:00')).toBe(36);
     });
+
+    it("should automatically differentate pay rate for after bedtime", function(){
+        expect(CalculatePayment('5:00', '8:00', '10:00')).toBe(52);
+    })
   });
